eeade9f200976a51bd3005a3f85b5bed97b61189
aafgani <andrya.afgani@gmail.com>
2022-09-16 17:35:10 +0700

    tugas pertememuan 2

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (filter) => {
+  let ixx = await promiseTheaterIXX();
+  let vgc = await promiseTheaterVGC();
+
+  return (
+    ixx.filter((i) => i.hasil === filter).length +
+    vgc.filter((i) => i.hasil === filter).length
+  );
+};
 
 module.exports = {
   promiseOutput,
